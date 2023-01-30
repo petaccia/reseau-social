31731a6c02a36e1fa5f5116e316adaddcd4872e7
PETACCIA <petaccia.seb@gmail.com>
2023-01-30 12:44:35 +0100

    connexion à la base de données

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 // Importation de morgan(logger http)
 const morgan = require("morgan");
 
+// Importation du fichier data.js pour connexion à la base de donnée
+const mongoose = require("./database/data");
+
 const app = express();
 
 //logger les requêtes et les reponses 
