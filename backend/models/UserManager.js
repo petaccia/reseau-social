77566b89a83f97617a7c49fcd5d4b3f84d3d1be8
PETACCIA <petaccia.seb@gmail.com>
2023-02-13 11:35:14 +0100

    mise en place de l'email unique

--- backend/models/UserManager.js
+++ backend/models/UserManager.js
@@ -1,10 +1,15 @@
 // importation de mongoose 
 const mongoose = require("mongoose");
 
+// importation mongoose-unique-validator
+const uniqueValidator = require("mongoose-unique-validator"); 
 //modèle de base de donnée pour le signup( pour enregistrer un nouvel utlisateur )
 const userSchema = mongoose.Schema({
   email : { type: String, required: true, unique: true },
   password : { type: String, required: true }
-})
+});
+
+// sécurité conseillé pour ne pas enregistrer 2 fois la même adresse email dans la base de donnée
+userSchema.plugin(uniqueValidator);
 //exportation du module
 module.exports = mongoose.model("user", userSchema);
