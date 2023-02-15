5c69688a5652f7cdca538e30197b4ecb52448387
PETACCIA <petaccia.seb@gmail.com>
2023-02-15 11:53:08 +0100

    création du schema pour les users

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,18 +1,6 @@
 // importation express
 const express = require("express");
 
-//Importation du middleware/password
-const password = require("../middleware/password")
-
-//Importation du controllers
-const userController = require("../controllers/UserControllers");
 
 //La fonction router
 const router = express.Router();
-
-// La route login
-router.post("/login", userController.login)
-
-// la route (endpoint) signUp
-router.post("/signUp", password, userController.signUp)
-module.exports = router;
